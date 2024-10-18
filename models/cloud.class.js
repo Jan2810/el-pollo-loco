1fa196dcd7655ece972b5e1dc4ad6f83892c78db
Jan Steiner <jansteiner73@gmail.com>
2024-10-18 18:17:52 +0200

    first animations

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -6,6 +6,13 @@ class Cloud extends MovableObject {
 
     constructor() {
         super().loadImage('img/5_background/layers/4_clouds/1.png');
-        this.x  = 0;
+        this.x  = Math.random() * 200;
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            this.x -= 0.1;
+        },1000/60);
     }
 }
